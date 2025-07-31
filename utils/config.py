27a50d766fd335f8b0ce4ce6c9fc69db6cb08f74
Insun35 <inseonyu7@gmail.com>
2025-07-31 09:46:08 +0900

    fix: change api base to raspberrypi hosting server

--- utils/config.py
+++ utils/config.py
@@ -5,4 +5,4 @@ load_dotenv()
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 DB_PATH = os.getenv("DB_PATH", "action_log.db")
-API_BASE = "http://localhost:8080"
+API_BASE = "http://raspberrypi4.local:8080"
